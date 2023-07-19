a084fb86032e449536ea2bdba5aa0f17eed5ff9c
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-07-19 19:54:21 -0300

    Criado objeto Product, criado atributos e feito toString

--- src/app/Program.java
+++ src/app/Program.java
@@ -6,10 +6,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import entities.Product;
 
 public class Program {
 
 	public static void main(String[] args) throws SQLException {
+		
 		
 		Connection conn = DB.getConnection();
 	
@@ -18,7 +20,16 @@ public class Program {
 		ResultSet rs = st.executeQuery("select * from tb_product");
 			
 		while (rs.next()) {
-			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+
+			Product p = new Product();
+
+			p.setId(rs.getLong("id"));
+			p.setDescription(rs.getString("description"));
+			p.setName(rs.getString("name"));
+			p.setImageUri(rs.getString("image_uri"));
+			p.setPrice(rs.getDouble("price"));
+
+			System.out.println(p);
 		}
 	}
 }
