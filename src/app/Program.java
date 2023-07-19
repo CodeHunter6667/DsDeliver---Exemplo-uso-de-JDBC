c5a8e74d8625bdc63db2919195554b14c789e34f
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-07-19 20:50:38 -0300

    Criado HashMap para comparar dados do DB e inserir os que não se repetem

--- src/app/Program.java
+++ src/app/Program.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 
 import db.DB;
 import entities.Order;
@@ -23,11 +25,33 @@ public class Program {
 				+ "INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id "
 				+ "INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id");
 			
+		Map<Long, Order> map = new HashMap<>();
+		Map<Long, Product> prods = new HashMap<>();
 		while (rs.next()) {
+			
+			Long orderId = rs.getLong("order_id");
+			if (map.get(orderId) == null) {
 
 			Order order = instantiateOrder(rs);
+			map.put(orderId, order);
+			}
 
-			System.out.println(order);
+			Long productId = rs.getLong("product_id");
+
+			if (prods.get(productId) == null) {
+			Product p = instantiateProduct(rs);
+			prods.put(productId, p);	
+			}
+
+			map.get(orderId).getProducts().add(prods.get(productId));
+		}
+
+		for (Long orderId : map.keySet()) {
+			System.out.println(map.get(orderId));
+			for (Product p : map.get(orderId).getProducts()) {
+				System.out.println(p);
+			}
+			System.out.println();
 		}
 	}
 
@@ -35,7 +59,7 @@ public class Program {
 			
 		Product p = new Product();
 
-			p.setId(rs.getLong("id"));
+			p.setId(rs.getLong("product_id"));
 			p.setDescription(rs.getString("description"));
 			p.setName(rs.getString("name"));
 			p.setImageUri(rs.getString("image_uri"));
@@ -47,7 +71,7 @@ public class Program {
 			
 		Order order = new Order();
 
-			order.setId(rs.getLong("id"));
+			order.setId(rs.getLong("order_id"));
 			order.setLatitude(rs.getDouble("latitude"));
 			order.setLongitude(rs.getDouble("longitude"));
 			order.setMoment(rs.getTimestamp("moment").toInstant());
