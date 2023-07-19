07cb488871506274d97606ca09072e78edc76371
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-07-19 20:19:31 -0300

    Criado objeto Order, feito metodo no main para instanciar  atributos, criado Enum para objeto Order

--- src/app/Program.java
+++ src/app/Program.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import entities.Order;
+import entities.OrderStatus;
 import entities.Product;
 
 public class Program {
@@ -17,13 +19,13 @@ public class Program {
 	
 		Statement st = conn.createStatement();
 			
-		ResultSet rs = st.executeQuery("select * from tb_product");
+		ResultSet rs = st.executeQuery("select * from tb_order");
 			
 		while (rs.next()) {
 
-			Product p = instantiateProduct(rs);
+			Order order = instantiateOrder(rs);
 
-			System.out.println(p);
+			System.out.println(order);
 		}
 	}
 
@@ -37,5 +39,17 @@ public class Program {
 			p.setImageUri(rs.getString("image_uri"));
 			p.setPrice(rs.getDouble("price"));
 			return p;
+	}
+
+	private static Order instantiateOrder(ResultSet rs) throws SQLException{
+			
+		Order order = new Order();
+
+			order.setId(rs.getLong("id"));
+			order.setLatitude(rs.getDouble("latitude"));
+			order.setLongitude(rs.getDouble("longitude"));
+			order.setMoment(rs.getTimestamp("moment").toInstant());
+			order.setStatus(OrderStatus.values()[rs.getInt("status")]);
+			return order;
 	}
 }
